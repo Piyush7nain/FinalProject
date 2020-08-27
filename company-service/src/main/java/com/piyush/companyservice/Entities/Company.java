85a1016aa9707e2be0dbba4e349dbbfa5b42f7fa
Piyush Nain <piyush7nain@gmail.com>
2020-08-27 08:53:05 +0530

    Added Stock functionality to company service

--- company-service/src/main/java/com/piyush/companyservice/Entities/Company.java
+++ company-service/src/main/java/com/piyush/companyservice/Entities/Company.java
@@ -1,25 +1,23 @@
 package com.piyush.companyservice.Entities;
 
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Company {
 
     @Id
-    @Column(name = "id")
-    private Integer id;
+    @Column(name = "code")
+    private Integer code;
     
     @Column(name ="company_name", nullable = false)
     private String companyName;
@@ -30,5 +28,66 @@ public class Company {
     @Column(name ="company_details")
     private String companyDetails;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "company", fetch = FetchType.LAZY)
+    @JsonIgnoreProperties("company")
+    private List<StockPrices> stockPrices;
+
+    public Integer getId() {
+        return code;
+    }
+
+    public void setId(Integer id) {
+        this.code = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public double getTurnover() {
+        return turnover;
+    }
+
+    public void setTurnover(double turnover) {
+        this.turnover = turnover;
+    }
+
+    public String getCompanyDetails() {
+        return companyDetails;
+    }
+
+    public void setCompanyDetails(String companyDetails) {
+        this.companyDetails = companyDetails;
+    }
+
+    public Company() {
+    }
+
+    public Company(Integer id, String companyName, double turnover, String companyDetails) {
+        this.code = id;
+        this.companyName = companyName;
+        this.turnover = turnover;
+        this.companyDetails = companyDetails;
+    }
+
+    public List<StockPrices> getStockPrices() {
+        return stockPrices;
+    }
+
+    public void addStockPrices(StockPrices stockPrices) {
+        this.stockPrices.add( stockPrices);
+    }
+
+    /* @Override
+    public String toString() {
+        return "Company [code=" + code + ", companyDetails=" + companyDetails + ", companyName=" + companyName
+                + ", turnover=" + turnover + "]";
+    } */
+
+    
     
 }
--- user-service/src/main/java/com/piyush/UserService/repo/UserRepository.java
+++ user-service/src/main/java/com/piyush/UserService/repo/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import com.piyush.UserService.model.User;
 
-@Repository
+@Repository 
 public interface UserRepository extends JpaRepository<User,Integer> {
 	public User findUserByUserId(String userId);
 }
